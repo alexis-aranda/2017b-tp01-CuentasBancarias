435788d57b3aa665a52908dbeab1289b87b12b37
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-07 20:50:16 -0300

    ROJO: Definimos la clase

---
+++ Test/cuentasBancarias/CuentaBancariaTest.java
@@ -0,0 +1,15 @@
+package cuentasBancarias;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class CuentaBancariaTest {
+
+	@Test
+	public void queInicia() {
+		CuentaBancaria c= new CuentaBancaria();
+		Assert.assertEquals(0, c.mostrar(),0);
+	}
+
+}
--- src/cuentasBancarias/CuentaBancaria.java
+++ src/cuentasBancarias/CuentaBancaria.java
@@ -1,5 +1,17 @@
 package cuentasBancarias;
 
 public class CuentaBancaria {
+	
+	private double fondos;
+	
+	public CuentaBancaria() {
+		this.fondos=1;
+	}
 
+	public double mostrar() {
+		// TODO Auto-generated method stub
+		return this.fondos;
+	}
+
+	
 }
