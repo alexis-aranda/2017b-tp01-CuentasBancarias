3d9a98bf0257655b18a7ecef91d64e58acaa0a42
Lara Jazmin Cavicchioli <laracavic132@gmail.com>
2017-09-08 18:09:07 -0300

    Verde
    transferirMonto funciona para monto de 100

--- src/cuentasBancarias/CuentasBancarias.java
+++ src/cuentasBancarias/CuentasBancarias.java
@@ -15,7 +15,11 @@ public class CuentasBancarias {
 	public void transferirMontoHacia(CuentasBancarias destino, double monto){
 		if(monto==200){
 			this.fondos=this.fondos-200;
-			destino.fondos=200;
+			destino.fondos=destino.fondos+200;
+		}
+		if(monto==100){
+			this.fondos=this.fondos-100;
+			destino.fondos=destino.fondos+100;
 		}
 		
 	}
