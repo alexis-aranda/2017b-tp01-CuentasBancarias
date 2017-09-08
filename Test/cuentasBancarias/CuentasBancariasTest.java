2a2ac587f7709d19153910ce437df70e13bc1773
Lara Jazmin Cavicchioli <laracavic132@gmail.com>
2017-09-08 16:57:23 -0300

    Rojo
    testeo de constructor con parametro

--- Test/cuentasBancarias/CuentasBancariasTest.java
+++ Test/cuentasBancarias/CuentasBancariasTest.java
@@ -9,7 +9,10 @@ public class CuentasBancariasTest {
 	public void queInicia(){
 		CuentasBancarias nuevo = new CuentasBancarias();
 		Assert.assertEquals(0,0,nuevo.mostrar());
+		CuentasBancarias nuevo = new CuentasBancarias(500);
+		Assert.assertEquals(500,nuevo.mostrar());
 	}
+	
 	
 	public static void main(String[] args) {
 		
